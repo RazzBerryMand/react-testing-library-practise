3fe47b5e8ffc62105ea698cdfc39bb5ec11f43d3
Mand Cashin <mand.cashin@deptagency.com>
2022-04-06 00:51:36 +0100

    all tests passing

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Counter() {
-const [counterValue, setCounterValue] = useState(0);
-
+  const [counterValue, setCounterValue] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+;
   return (
     <div>
-    <h3 data-testid="header">My Counter</h3>
-    <h2 data-testid="counter">{counterValue}</h2>
+      <h3 data-testid="header">My Counter</h3>
+      <h2 data-testid="counter">{counterValue}</h2>
+      <button data-testid="subtract-btn">-</button>
+      <input data-testid="input" type="number" value={inputValue} />
+      <button data-testid="add-btn">+</button>
     </div>
   );
 }
